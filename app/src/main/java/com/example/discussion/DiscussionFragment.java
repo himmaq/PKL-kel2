edf3ed46b09b181d7f4d5b572e7095264bbcc2be
zalfar14 <zalfarossana1234@gmail.com>
2021-08-24 16:59:58 +0700

    penambahan ui fragment_select_child.xml, fragment_add_children.xml tombol masing-masing topik di DiscussionFragment

--- app/src/main/java/com/example/discussion/DiscussionFragment.java
+++ app/src/main/java/com/example/discussion/DiscussionFragment.java
@@ -11,14 +11,55 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class DiscussionFragment extends Fragment {
+    ImageView ivInfants, ivPregnancy, ivEducation, ivBehaviour, ivToddlers, ivPreschool, ivParenting, ivTough;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_discussion, container, false);
+
+        ivInfants = view.findViewById(R.id.infants_img);
+        ivPregnancy = view.findViewById(R.id.pregnancy_img);
+        ivEducation = view.findViewById(R.id.edu_img);
+        ivBehaviour = view.findViewById(R.id.behavior_img);
+        ivToddlers = view.findViewById(R.id.toodlers_img);
+        ivPreschool = view.findViewById(R.id.preschool_img);
+        ivParenting = view.findViewById(R.id.parenting_img);
+        ivTough = view.findViewById(R.id.tough_img);
+
+        ivInfants.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+
+        ivPregnancy.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+        ivEducation.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+
+        ivBehaviour.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+        ivToddlers.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+
+        ivPreschool.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+        ivParenting.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+        ivTough.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+
+
         return view;
     }
 
---
+++ app/src/main/java/com/example/discussion/SelectChildFragment.java
@@ -0,0 +1,26 @@
+package com.example.discussion;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SelectChildFragment extends Fragment {
+    Button btnAddChild;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_discussion, container, false);
+
+        btnAddChild = view.findViewById(R.id.btn_add_child);
+        btnAddChild.setOnClickListener(view1-> {
+            getFragmentManager().beginTransaction().replace(R.id.fl_fragment, new MenuDiscussionFragment()).commit();
+        });
+       return view;
+    }
+}
